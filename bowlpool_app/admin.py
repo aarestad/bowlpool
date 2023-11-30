acbf24ed02076679bb5c3b421d480e8af609d10a
Peter Aarestad <aarestad@gmail.com>
2023-11-30 15:54:24 -0600

    register BowlMatchupPick in admin

--- bowlpool_app/admin.py
+++ bowlpool_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Team, BowlGame, BowlMatchup
+from .models import Team, BowlGame, BowlMatchup, BowlMatchupPick
 
 admin.site.register(Team)
 admin.site.register(BowlGame)
 admin.site.register(BowlMatchup)
+admin.site.register(BowlMatchupPick)
