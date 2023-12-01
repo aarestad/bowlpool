1ecbe3a132eba23a0a3d6120c6f60849a15264f0
Peter Aarestad <aarestad@gmail.com>
2023-12-01 16:03:23 -0600

    forward root to bowl-pool root

--- bowlpool/urls.py
+++ bowlpool/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = (
@@ -11,11 +11,7 @@ urlpatterns = (
         path("bowl-pool/", include("bowlpool_app.urls")),
         path("accounts/", include("allauth.urls")),
         path("logout", LogoutView.as_view()),
-        path(
-            "",
-            TemplateView.as_view(template_name="static_pages/index.html"),
-            name="home",
-        ),
+        path("", RedirectView.as_view(url="bowl-pool/")),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
